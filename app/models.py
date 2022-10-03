62fc2716eab22ee8cabcd0904dbb52179d742e24
ibanesss121232 <105462844+ibanesss121232@users.noreply.github.com>
2022-10-02 22:08:27 -0500

    Sumary

--- app/models.py
+++ app/models.py
@@ -1,1 +1,20 @@
 from app import db
+import email
+from datetime import datetime
+from encodings import search_function
+from msilib.schema import InstallUISequence
+from pickle import FALSE
+
+#La funcion User nos ayudara para el login del usuario y luego posteriormente
+#pasarlo a nuestra base de datos
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+
+    #permite imprimir el objeto usuario y mostrar datos
+    def __repr__(self):
+        return '<user {}>'.format(self.username, self.email)
+
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,42 @@
-from app import app
-import re
-from flask import render_template, request
-from app import db
+from app import app #importamos nuestra app
+import re #para manipular regular expresiones
+from flask import render_template, request #las importar para usarlo en nuestra ruta
+
+from app import db #para mandar solicitudes
+
+#los modelos siguientes nos ayudan a manejar los apis
 import requests
 import json
 
+#importamos los modelosque vamos utilizar en nuetras rutas
+from app.models import User
+
+#para manipular fechas
+from datetime import datetime
+
+#varias rutas pueden ser defenidas por la misma funcion
+
 @app.route("/")
+@app.route('/index', methods=['GET'])
+#la funcionedefine el comportamiento del servidor al ser solicitado
+ 
 def index():
-    return render_template("index.html")
+    user={'username':'usuario'}
+
+    return render_template("index.html", title='Moodsica app',user=user, elemento1='codigo',elemento2='HTML')
+
+
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+#En esta parte es la parte de login del usuario
+
+@app.route("/registrate/", methods=["GET", "POST"])
+def registrate():
+     if request.method == "POST":
+         username = request.form["username"]
+         password = request.form["password"]
+         return "login attempt for username " + username
+     return render_template("Registrate.html")
