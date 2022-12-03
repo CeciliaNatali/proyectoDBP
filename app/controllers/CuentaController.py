5893e140d5578e93417bc65f09f2753dd808a964
CeciliaNatali <cecilia.pilco@utec.edu.pe>
2022-12-03 11:42:49 -0500

    Final commit

--- app/controllers/CuentaController.py
+++ app/controllers/CuentaController.py
@@ -1,10 +1,11 @@
 from app import app
 import re
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify
 from app import db
 from app.models import Usuario
 import requests
 import json
+
 from cryptography.hazmat.primitives import hashes
 
 def cuenta():
@@ -35,33 +36,49 @@ def eliminar():
         return json.dumps({"success": False})
 
 def actualizar():
-    if request.method == "POST":
-        usuario = request.form["usuario"]
-        viejaContra = request.form["viejaContra"]
-        nuevaContra = request.form["nuevaContra"]
+    body = request.get_json()
+    usuario = body["usuario"]
+    viejaContra = body["viejaContra"]
+    nuevaContra = body["nuevaContra"]
 
-        #if not viejaContra and not viejoUsuario:
-        #    return "Requiere al menos un parámetro a actualizar."
+    usuario1 = Usuario.query.filter(Usuario.nombre==usuario).first()
+    if usuario1 == None or usuario1.contrasenna != viejaContra:
+        return json.dumps({"success": False})
 
-        usuario1 = Usuario.query.filter(Usuario.nombre==usuario).first()
-        if usuario1 == None or usuario1.contrasenna != viejaContra:
-            return "Usuario inválido y/o contraseña inválidos."
-        
-        if usuario1 and (usuario1.contrasenna == viejaContra):
-            usuario1.contrasenna = nuevaContra
-        try:
-            db.session.commit()
-        except Exception as err:
-            print("Error al actualizar ", err)
-            return "Error interno del servidor al actualizar, porfavor intente nuevamente."
-        return redirect("/cuenta?nombre=" + usuario1.nombre)
-    return render_template("actualizar.html")
+    if usuario1 and (usuario1.contrasenna == viejaContra):
+        usuario1.contrasenna = nuevaContra
+    
+    try:
+        db.session.commit()
+        return json.dumps({"success": True})
+    except Exception as err:
+        print(err)
+        return json.dumps({"success": False})
     
 def buscador():
-    return render_template("buscador.html")
+    body = request.get_json()
+    tag    = body["tag"]
+    yearmin = body["yearmin"]
+    yearmax = body["yearmax"]
+    #print(body)
+
+    url = "http://musicovery.com/api/V6/playlist.php?&fct=getfromtag&tag="+tag+"&yearmin="+yearmin+"&yearmax="+yearmax
+    result = requests.get(url, verify=False).json()
+    result = result["tracks"]["track"]
+    #print(type(result))
+    nuevaLista = []
+    for element in result:
+        nuevaLista.append({"titulo":element["title"],"artista": element["artist_display_name"], "genero":element["genre"]})
+        #print(element.keys())
+    #print(nuevaLista)
+
+    #print(json.dumps(result, indent=4))
+    return jsonify({"success": True, "result": nuevaLista})
+
+
+
+
 def playlist():
-    pass
-def buscar():
     pass
 def fondo():
     pass
--- app/routes/cuenta_bp.py
+++ app/routes/cuenta_bp.py
@@ -5,7 +5,7 @@ from app.controllers.CuentaController import cuenta, buscador, fondo, playlist, 
 cuenta_bp= Blueprint('cuenta_bp', __name__)
 
 cuenta_bp.route("/", methods=["GET"]) (cuenta)
-cuenta_bp.route("/buscador", methods=["GET"]) (buscador)
+cuenta_bp.route("/buscador", methods=["POST"]) (buscador)
 cuenta_bp.route("/playlist", methods=["GET"]) (playlist)
 cuenta_bp.route("/fondo", methods=["GET", "POST"]) (fondo)
 cuenta_bp.route("/salir", methods=["GET"]) (salir)
